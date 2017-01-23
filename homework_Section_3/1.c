61e2e1e5e9c2afb916f707e864f6057d95f13fd4
qiaturez <aturez@outlook.com>
2017-01-22 21:04:07 -0800

    first commit

---
+++ homework_Section_3/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main (void)
+{
+    int temp;
+    printf("Please enter a ASCII value : ");
+    printf(" __\b\b");
+    scanf("%d", &temp);
+    printf("The value is : %c \n",temp);
+    printf("\a Startled by the sudden sound,Sally shouted,\"By the Great pumpkin,what was that !\" ");
+
+    return 0;
+}
